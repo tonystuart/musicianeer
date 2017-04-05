d771c726258e270e422f19c20ec2e4108689b243
Tony Stuart <anthonyfstuart@gmail.com>
2017-04-04 23:06:35 -0700

    Add flexible conditional logic to configuration

--- MusicPad/src/com/example/afs/musicpad/device/midi/MidiConfiguration.java
+++ MusicPad/src/com/example/afs/musicpad/device/midi/MidiConfiguration.java
@@ -13,64 +13,251 @@ import java.util.LinkedList;
 
 public class MidiConfiguration {
 
-  public static class ConfigurationMessage {
-    private int command;
-    private int channel;
-    private int data1;
-    private int data2;
+  public static class Action {
 
-    public int getChannel() {
-      return channel;
+    private Integer setShift;
+    private Integer clearShift;
+    private Integer setMode;
+    private Integer clearMode;
+    private LinkedList<ChannelMessage> sendDeviceMessages;
+    private LinkedList<ChannelMessage> sendHandlerMessages;
+    private LinkedList<Command> sendHandlerCommands;
+
+    public Integer getClearMode() {
+      return clearMode;
     }
 
-    public int getCommand() {
-      return command;
+    public Integer getClearShift() {
+      return clearShift;
     }
 
-    public int getData1() {
-      return data1;
+    public LinkedList<ChannelMessage> getSendDeviceMessages() {
+      return sendDeviceMessages;
     }
 
-    public int getData2() {
-      return data2;
+    public LinkedList<Command> getSendHandlerCommands() {
+      return sendHandlerCommands;
     }
 
-    public void setChannel(int channel) {
-      this.channel = channel;
+    public LinkedList<ChannelMessage> getSendHandlerMessages() {
+      return sendHandlerMessages;
     }
 
-    public void setCommand(int command) {
-      this.command = command;
+    public Integer getSetMode() {
+      return setMode;
     }
 
-    public void setData1(int data1) {
-      this.data1 = data1;
+    public Integer getSetShift() {
+      return setShift;
     }
 
-    public void setData2(int data2) {
-      this.data2 = data2;
+    public void setClearMode(Integer clearMode) {
+      this.clearMode = clearMode;
+    }
+
+    public void setClearShift(Integer clearShift) {
+      this.clearShift = clearShift;
+    }
+
+    public void setSendDeviceMessages(LinkedList<ChannelMessage> sendDeviceMessages) {
+      this.sendDeviceMessages = sendDeviceMessages;
+    }
+
+    public void setSendHandlerCommands(LinkedList<Command> sendHandlerCommands) {
+      this.sendHandlerCommands = sendHandlerCommands;
+    }
+
+    public void setSendHandlerMessages(LinkedList<ChannelMessage> sendHandlerMessages) {
+      this.sendHandlerMessages = sendHandlerMessages;
+    }
+
+    public void setSetMode(Integer setMode) {
+      this.setMode = setMode;
+    }
+
+    public void setSetShift(Integer setShift) {
+      this.setShift = setShift;
     }
 
     @Override
     public String toString() {
-      return String.format("ConfigurationMessage [command=%d (0x%02x), channel=%d (0x%02x), data1=%d (0x%02x), data2=%d (0x%02x)", command, command, channel, channel, data1, data1, data2, data2);
+      return "Action [setShift=" + setShift + ", clearShift=" + clearShift + ", setMode=" + setMode + ", clearMode=" + clearMode + ", sendDeviceMessages=" + sendDeviceMessages + ", sendHandlerMessages=" + sendHandlerMessages + ", sendHandlerCommands=" + sendHandlerCommands + "]";
     }
 
   }
 
-  private LinkedList<ConfigurationMessage> initializers;
+  public static class ChannelMessage {
 
-  public LinkedList<ConfigurationMessage> getInitializers() {
+    private Integer subDevice;
+    private Integer command;
+    private Integer channel;
+    private Integer data1;
+    private Integer data2;
+
+    public boolean equals(int subDevice, int command, int channel, int data1, int data2) {
+      return (this.subDevice == null || this.subDevice == subDevice) && //
+          (this.command == null || this.command == command) && //
+          (this.channel == null || this.channel == channel) && //
+          (this.data1 == null || this.data1 == data1) && //
+          (this.data2 == null || this.data2 == data2);
+    }
+
+    public Integer getChannel() {
+      return channel;
+    }
+
+    public Integer getCommand() {
+      return command;
+    }
+
+    public Integer getData1() {
+      return data1;
+    }
+
+    public Integer getData2() {
+      return data2;
+    }
+
+    public Integer getSubDevice() {
+      return subDevice;
+    }
+
+    public void setChannel(Integer channel) {
+      this.channel = channel;
+    }
+
+    public void setCommand(Integer command) {
+      this.command = command;
+    }
+
+    public void setData1(Integer data1) {
+      this.data1 = data1;
+    }
+
+    public void setData2(Integer data2) {
+      this.data2 = data2;
+    }
+
+    public void setSubDevice(Integer subdevice) {
+      this.subDevice = subdevice;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Output [command=%d (0x%02x), channel=%d (0x%02x), data1=%d (0x%02x), data2=%d (0x%02x)", command, command, channel, channel, data1, data1, data2, data2);
+    }
+
+  }
+
+  public static class Command {
+
+    private Integer command;
+    private Integer parameter;
+
+    public Integer getCommand() {
+      return command;
+    }
+
+    public Integer getParameter() {
+      return parameter;
+    }
+
+    public void setCommand(Integer command) {
+      this.command = command;
+    }
+
+    public void setParameter(Integer parameter) {
+      this.parameter = parameter;
+    }
+
+    @Override
+    public String toString() {
+      return "Command [command=" + command + ", parameter=" + parameter + "]";
+    }
+
+  }
+
+  public static class Input {
+
+    private ChannelMessage ifMatch;
+    private Integer andIfMode;
+    private Integer andIfShift;
+    private Action onPress;
+    private Action onRelease;
+
+    public boolean equals(int subdevice, int command, int channel, int data1, int data2) {
+      return ifMatch.equals(subdevice, command, channel, data1, data2);
+    }
+
+    public Integer getAndIfMode() {
+      return andIfMode;
+    }
+
+    public Integer getAndIfShift() {
+      return andIfShift;
+    }
+
+    public ChannelMessage getIfMatch() {
+      return ifMatch;
+    }
+
+    public Action getOnPress() {
+      return onPress;
+    }
+
+    public Action getOnRelease() {
+      return onRelease;
+    }
+
+    public void setAndIfMode(Integer andIfMode) {
+      this.andIfMode = andIfMode;
+    }
+
+    public void setAndIfShift(Integer andIfShift) {
+      this.andIfShift = andIfShift;
+    }
+
+    public void setIfMatch(ChannelMessage ifMatch) {
+      this.ifMatch = ifMatch;
+    }
+
+    public void setOnPress(Action onPress) {
+      this.onPress = onPress;
+    }
+
+    public void setOnRelease(Action onRelease) {
+      this.onRelease = onRelease;
+    }
+
+    @Override
+    public String toString() {
+      return "Input [ifMatch=" + ifMatch + ", andIfMode=" + andIfMode + ", andIfShift=" + andIfShift + ", onPress=" + onPress + ", onRelease=" + onRelease + "]";
+    }
+
+  }
+
+  private LinkedList<ChannelMessage> initializers;
+  private LinkedList<Input> inputs;
+
+  public LinkedList<ChannelMessage> getInitializers() {
     return initializers;
   }
 
-  public void setInitializers(LinkedList<ConfigurationMessage> initializers) {
+  public LinkedList<Input> getInputs() {
+    return inputs;
+  }
+
+  public void setInitializers(LinkedList<ChannelMessage> initializers) {
     this.initializers = initializers;
+  }
+
+  public void setInputs(LinkedList<Input> inputs) {
+    this.inputs = inputs;
   }
 
   @Override
   public String toString() {
-    return "MidiConfiguration [initializers=" + initializers + "]";
+    return "MidiConfiguration [initializers=" + initializers + ", inputs=" + inputs + "]";
   }
 
 }
--- MusicPad/src/com/example/afs/musicpad/device/midi/MidiReader.java
+++ MusicPad/src/com/example/afs/musicpad/device/midi/MidiReader.java
@@ -11,6 +11,9 @@ package com.example.afs.musicpad.device.midi;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 
 import javax.sound.midi.InvalidMidiDataException;
@@ -21,8 +24,12 @@ import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
 
 import com.example.afs.musicpad.device.common.DeviceGroup.DeviceInterface;
-import com.example.afs.musicpad.device.midi.MidiConfiguration.ConfigurationMessage;
+import com.example.afs.musicpad.device.midi.MidiConfiguration.Action;
+import com.example.afs.musicpad.device.midi.MidiConfiguration.ChannelMessage;
+import com.example.afs.musicpad.device.midi.MidiConfiguration.Command;
+import com.example.afs.musicpad.device.midi.MidiConfiguration.Input;
 import com.example.afs.musicpad.message.Message;
+import com.example.afs.musicpad.message.OnCommand;
 import com.example.afs.musicpad.message.OnInputPress;
 import com.example.afs.musicpad.message.OnInputRelease;
 import com.example.afs.musicpad.util.DirectList;
@@ -55,18 +62,15 @@ public class MidiReader implements DeviceInterface {
   private RandomAccessList<Receiver> receivers = new DirectList<>();
   private MidiConfiguration configuration;
 
+  private Set<Integer> currentShifts = new HashSet<>();
+  private Set<Integer> currentModes = new HashSet<>();
+
   public MidiReader(BlockingQueue<Message> queue, MidiDeviceBundle device) {
     this.queue = queue;
     this.device = device;
     this.configuration = readConfiguration();
     connectDevices();
     initializeDevices();
-  }
-
-  public void sendToDevice(MidiMessage message, long timestamp) {
-    for (Receiver receiver : receivers) {
-      receiver.send(message, timestamp);
-    }
   }
 
   @Override
@@ -94,19 +98,16 @@ public class MidiReader implements DeviceInterface {
     }
   }
 
+  private int getInt(Integer value) {
+    return getInt(value, 0);
+  }
+
+  private int getInt(Integer value, int defaultValue) {
+    return value == null ? defaultValue : value;
+  }
+
   private void initializeDevices() {
-    for (ConfigurationMessage message : configuration.getInitializers()) {
-      try {
-        int command = message.getCommand();
-        int channel = message.getChannel();
-        int data1 = message.getData1();
-        int data2 = message.getData2();
-        ShortMessage shortMessage = new ShortMessage(command, channel, data1, data2);
-        sendToDevice(shortMessage, -1);
-      } catch (InvalidMidiDataException e) {
-        throw new RuntimeException(e);
-      }
-    }
+    sendDeviceMessages(configuration.getInitializers());
   }
 
   private MidiConfiguration readConfiguration() {
@@ -127,15 +128,90 @@ public class MidiReader implements DeviceInterface {
     return new MidiConfiguration();
   }
 
-  private void receiveFromDevice(MidiMessage message, long timestamp, int subdevice) {
+  private void receiveFromDevice(MidiMessage message, long timestamp, int subDevice) {
     if (message instanceof ShortMessage) {
       ShortMessage shortMessage = (ShortMessage) message;
-      int type = shortMessage.getCommand();
-      int control = shortMessage.getData1();
-      if (type == ShortMessage.NOTE_ON) {
-        queue.add(new OnInputPress(control));
-      } else if (type == ShortMessage.NOTE_OFF) {
-        queue.add(new OnInputRelease(control));
+      int command = shortMessage.getCommand();
+      int channel = shortMessage.getChannel();
+      int data1 = shortMessage.getData1();
+      int data2 = shortMessage.getData2();
+      for (Input input : configuration.getInputs()) {
+        if (input.equals(subDevice, command, channel, data1, data2)) {
+          if (input.getAndIfShift() == null || currentShifts.contains(input.getAndIfShift())) {
+            if (input.getAndIfMode() == null || currentModes.contains(input.getAndIfMode())) {
+              Action action;
+              if (command == ShortMessage.NOTE_ON) {
+                action = input.getOnPress();
+              } else {
+                action = input.getOnRelease();
+              }
+              if (action.getSetShift() != null) {
+                currentShifts.add(action.getSetShift());
+              }
+              if (action.getClearShift() != null) {
+                currentShifts.remove(action.getClearShift());
+              }
+              if (action.getSetMode() != null) {
+                currentModes.add(action.getSetMode());
+              }
+              if (action.getClearMode() != null) {
+                currentModes.remove(action.getClearMode());
+              }
+              sendDeviceMessages(action.getSendDeviceMessages());
+              sendHandlerMessages(action.getSendHandlerMessages(), true);
+              sendHandlerCommands(action.getSendHandlerCommands());
+              return; // TODO: Rethink this...
+            }
+          }
+        }
+      }
+      if (command == ShortMessage.NOTE_ON) {
+        queue.add(new OnInputPress(data1));
+      } else if (command == ShortMessage.NOTE_OFF) {
+        queue.add(new OnInputRelease(data1));
+      }
+    }
+  }
+
+  private void sendDeviceMessages(List<ChannelMessage> deviceMessages) {
+    for (ChannelMessage channelMessage : deviceMessages) {
+      try {
+        int command = channelMessage.getCommand();
+        int channel = channelMessage.getChannel();
+        int data1 = getInt(channelMessage.getData1());
+        int data2 = getInt(channelMessage.getData2());
+        ShortMessage shortMessage = new ShortMessage(command, channel, data1, data2);
+        if (channelMessage.getSubDevice() == null) {
+          for (Receiver receiver : receivers) {
+            receiver.send(shortMessage, -1);
+          }
+        } else {
+          receivers.get(channelMessage.getSubDevice()).send(shortMessage, -1);
+        }
+      } catch (InvalidMidiDataException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private void sendHandlerCommands(List<Command> handlerCommands) {
+    for (Command command : handlerCommands) {
+      queue.add(new OnCommand(com.example.afs.musicpad.Command.values()[command.getCommand()], command.getParameter()));
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private void sendHandlerMessages(List<ChannelMessage> handlerMessages, boolean isPress) {
+    for (ChannelMessage channelMessage : handlerMessages) {
+      int command = channelMessage.getCommand();
+      int channel = channelMessage.getChannel();
+      int data1 = getInt(channelMessage.getData1());
+      int data2 = getInt(channelMessage.getData2());
+      // ShortMessage shortMessage = new ShortMessage(command, channel, data1, data2);
+      if (isPress) {
+        queue.add(new OnInputPress(data1));
+      } else {
+        queue.add(new OnInputRelease(data1));
       }
     }
   }
