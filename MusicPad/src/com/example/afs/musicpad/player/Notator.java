d47b9bd4a4436efa85e845d651d0bd6032c7d817
Tony Stuart <anthonyfstuart@gmail.com>
2017-05-14 10:15:17 -0700

    Clean up notator

--- MusicPad/src/com/example/afs/musicpad/player/Notator.java
+++ MusicPad/src/com/example/afs/musicpad/player/Notator.java
@@ -23,8 +23,53 @@ import com.example.afs.musicpad.svg.Svg;
 import com.example.afs.musicpad.svg.Text;
 import com.example.afs.musicpad.util.DirectList;
 import com.example.afs.musicpad.util.RandomAccessList;
+import com.example.afs.musicpad.util.Value;
 
 public class Notator {
+
+  private static class Context {
+    private NoteType noteType;
+    private Stem stem;
+    private int lowestMidiNote;
+    private int highestMidiNote;
+
+    public Context(NoteType noteType, Stem stem, int lowestMidiNote, int highestMidiNote) {
+      this.noteType = noteType;
+      this.stem = stem;
+      this.lowestMidiNote = lowestMidiNote;
+      this.highestMidiNote = highestMidiNote;
+    }
+
+    public int getHighestMidiNote() {
+      return highestMidiNote;
+    }
+
+    public int getLowestMidiNote() {
+      return lowestMidiNote;
+    }
+
+    public NoteType getNoteType() {
+      return noteType;
+    }
+
+    public Stem getStem() {
+      return stem;
+    }
+
+    @Override
+    public String toString() {
+      return "Context [duration=" + noteType + ", stem=" + stem + ", lowestMidiNote=" + lowestMidiNote + ", highestMidiNote=" + highestMidiNote + "]";
+    }
+
+  }
+
+  private enum NoteType {
+    EIGHTH, QUARTER, HALF, WHOLE
+  }
+
+  private enum Stem {
+    UP, DOWN
+  }
 
   private static final int LOWEST = 24;
   private static final int HIGHEST = 96;
@@ -38,7 +83,6 @@ public class Notator {
       74,
       77
   };
-
   private static final int[] BASS_MIDI_NOTES = new int[] {
       43,
       47,
@@ -46,7 +90,6 @@ public class Notator {
       53,
       57
   };
-
   private static final int[] LEDGER_MIDI_NOTES = new int[] {
       26,
       27,
@@ -89,11 +132,12 @@ public class Notator {
   private static final int RADIUS = 10; // spacing is r, diameter is 2r
   private static final int LEDGER_WIDTH = RADIUS * 2;
   private static final int INTER_CLEF = RADIUS * 6;
-
   private static final int TOP = RADIUS * 1;
   private static final int SPAN = (POSITION[HIGHEST] - POSITION[LOWEST]) + 1;
+
   private static final int BOTTOM = TOP + (SPAN * RADIUS) + INTER_CLEF;
   private static final int WORDS = BOTTOM / 2;
+
   private static final long RESOLUTION = Default.TICKS_PER_BEAT / 2;
 
   private static final String CLOSED = "closed";
@@ -134,91 +178,141 @@ public class Notator {
   }
 
   public String getMusic() {
-
-    if (song.getChannelNoteCount(channel) == 0) {
-      return "";
+    String music = "No music for channel " + Value.toNumber(channel);
+    if (song.getChannelNoteCount(channel) > 0) {
+      long duration = song.getDuration();
+      Svg staff = getStaff(duration);
+      drawMeasures(staff, duration);
+      long tick = 0;
+      TreeSet<Note> notes = song.getNotes(channel);
+      while (tick < duration) {
+        drawSlice(staff, notes, tick);
+        tick += RESOLUTION;
+      }
+      drawWords(staff);
+      music = staff.render();
     }
+    return music;
+  }
 
-    int top;
-    int highestMidiNote = song.getHighestMidiNote();
-    if (highestMidiNote < MIDDLE) {
-      top = getY(MIDDLE) + RADIUS;
-    } else {
-      top = getY(Math.max(highestMidiNote, TREBLE_MIDI_NOTES[4])) - RADIUS;
+  private void drawHead(Svg staff, Context context, int noteX, int noteY) {
+    switch (context.getNoteType()) {
+    case EIGHTH:
+    case QUARTER:
+      staff.add(new Circle(noteX, noteY, RADIUS, CLOSED));
+      break;
+    case HALF:
+    case WHOLE:
+      staff.add(new Circle(noteX, noteY, RADIUS, OPEN));
+      break;
+    default:
+      throw new UnsupportedOperationException();
     }
+  }
 
-    int bottom;
-    int lowestMidiNote = song.getLowestMidiNote();
-    if (lowestMidiNote < MIDDLE) {
-      bottom = getY(Math.min(lowestMidiNote, BASS_MIDI_NOTES[0])) + RADIUS;
-    } else {
-      bottom = getY(MIDDLE) + INTER_CLEF;
-    }
-
-    long duration = song.getDuration();
-    int width = scale(duration);
-    Svg svg = new Svg(0, top, width, bottom);
-
-    for (int i = 0; i < TREBLE_MIDI_NOTES.length; i++) {
-      int y = getY(TREBLE_MIDI_NOTES[i]);
-      svg.add(new Line(0, y, width, y));
-    }
-
-    for (int i = 0; i < BASS_MIDI_NOTES.length; i++) {
-      int y = getY(BASS_MIDI_NOTES[i]);
-      svg.add(new Line(0, y, width, y));
-    }
-
+  private void drawMeasures(Svg staff, long duration) {
     long tick = 0;
     while (tick < duration) {
       int x = scale(tick);
       if (tick > 0) {
         tick -= 2 * RADIUS; // so note doesn't land on it
       }
-      svg.add(new Line(x, getY(TREBLE_MIDI_NOTES[4]), x, getY(BASS_MIDI_NOTES[0])));
+      staff.add(new Line(x, getY(TREBLE_MIDI_NOTES[4]), x, getY(BASS_MIDI_NOTES[0])));
       tick += song.getTicksPerMeasure(tick);
     }
+  }
 
-    tick = 0;
-    TreeSet<Note> notes = song.getNotes(channel);
-    while (tick < duration) {
-      int midiNote = 0;
-      long firstTick = -1;
-      SortedSet<Note> slice = notes.subSet(new Note(tick), new Note(tick + RESOLUTION));
-      int sliceNoteCount = slice.size();
-      if (sliceNoteCount > 0) {
-        firstTick = slice.first().getTick();
-        RandomAccessList<Note> trebleNotes = new DirectList<>();
-        RandomAccessList<Note> bassNotes = new DirectList<>();
-        for (Note note : slice) {
-          midiNote = note.getMidiNote();
-          if (midiNote < MIDDLE) {
-            bassNotes.add(note);
-          } else {
-            trebleNotes.add(note);
-          }
+  private void drawNotes(Svg staff, long firstTick, RandomAccessList<Note> notes, int midPoint) {
+    if (notes.size() > 0) {
+      Context context = getContext(notes, midPoint);
+      for (Note note : notes) {
+        int midiNote = note.getMidiNote();
+        int noteX = scale(firstTick);
+        int noteY = getY(midiNote);
+        if (LEDGER[midiNote]) {
+          staff.add(new Line(noteX - LEDGER_WIDTH, noteY, noteX + LEDGER_WIDTH, noteY));
         }
-        plotNotes(svg, firstTick, trebleNotes, TREBLE_MIDI_NOTES[2]);
-        plotNotes(svg, firstTick, bassNotes, BASS_MIDI_NOTES[2]);
+        boolean isSharp = isSharp(midiNote);
+        if (isSharp) {
+          drawSharp(staff, noteX, noteY);
+        }
+        drawHead(staff, context, noteX, noteY);
       }
+      drawStem(staff, firstTick, context);
+    }
+  }
+
+  private void drawSharp(Svg staff, int noteX, int y) {
+    int full = RADIUS;
+    int half = RADIUS / 2;
+    int x = noteX - 3 * RADIUS;
+    staff.add(new Line(x - full, y - half, x + full, y - half));
+    staff.add(new Line(x - full, y + half, x + full, y + half));
+    staff.add(new Line(x - half, y - full, x - half, y + full));
+    staff.add(new Line(x + half, y - full, x + half, y + full));
+  }
+
+  private void drawSlice(Svg staff, TreeSet<Note> notes, long tick) {
+    int midiNote = 0;
+    long firstTick = -1;
+    SortedSet<Note> slice = notes.subSet(new Note(tick), new Note(tick + RESOLUTION));
+    int sliceNoteCount = slice.size();
+    if (sliceNoteCount > 0) {
+      firstTick = slice.first().getTick();
+      RandomAccessList<Note> trebleNotes = new DirectList<>();
+      RandomAccessList<Note> bassNotes = new DirectList<>();
+      for (Note note : slice) {
+        midiNote = note.getMidiNote();
+        if (midiNote < MIDDLE) {
+          bassNotes.add(note);
+        } else {
+          trebleNotes.add(note);
+        }
+      }
+      drawNotes(staff, firstTick, trebleNotes, TREBLE_MIDI_NOTES[2]);
+      drawNotes(staff, firstTick, bassNotes, BASS_MIDI_NOTES[2]);
       if (sliceNoteCount == 1) {
         String keyCap = player.toKeyCap(midiNote);
-        svg.add(new Text(scale(firstTick), WORDS + 3 * RADIUS, keyCap));
-      } else if (sliceNoteCount > 1) {
+        staff.add(new Text(scale(firstTick), WORDS + 3 * RADIUS, keyCap));
+      } else {
         Chord chord = new Chord(slice);
         String keyCap = player.toKeyCap(chord);
-        svg.add(new Text(scale(firstTick), WORDS + 3 * RADIUS, keyCap));
+        staff.add(new Text(scale(firstTick), WORDS + 3 * RADIUS, keyCap));
       }
-      tick += RESOLUTION;
     }
+  }
+
+  private void drawStem(Svg staff, long firstTick, Context context) {
+    if (context.getNoteType() != NoteType.WHOLE) {
+      int noteTop = getY(context.getHighestMidiNote());
+      int noteBottom = getY(context.getLowestMidiNote());
+      int stemLength = 5 * RADIUS;
+      int flagLength = 2 * RADIUS;
+      if (context.getStem() == Stem.UP) {
+        int x = scale(firstTick) + RADIUS;
+        int stemTop = noteTop - stemLength;
+        staff.add(new Line(x, stemTop, x, noteBottom));
+        if (context.getNoteType() == NoteType.EIGHTH) {
+          staff.add(new Line(x, stemTop, x + RADIUS, stemTop + flagLength));
+        }
+      } else {
+        int x = scale(firstTick) - RADIUS;
+        int stemBottom = noteBottom + stemLength;
+        staff.add(new Line(x, noteTop, x, stemBottom));
+        if (context.getNoteType() == NoteType.EIGHTH) {
+          staff.add(new Line(x, stemBottom, x - RADIUS, stemBottom - flagLength));
+        }
+      }
+    }
+  }
+
+  private void drawWords(Svg staff) {
     SortedSet<Word> words = song.getWords();
     for (Word word : words) {
       long wordTick = word.getTick();
       int wordX = scale(wordTick);
-      svg.add(new Text(wordX, WORDS, formatText(word.getText())));
+      staff.add(new Text(wordX, WORDS, formatText(word.getText())));
     }
-    String music = svg.render();
-    return music;
   }
 
   private String formatText(String text) {
@@ -231,6 +325,75 @@ public class Notator {
     return text;
   }
 
+  private int getBottom() {
+    int bottom;
+    int lowestMidiNote = song.getLowestMidiNote();
+    if (lowestMidiNote < MIDDLE) {
+      bottom = getY(Math.min(lowestMidiNote, BASS_MIDI_NOTES[0])) + RADIUS;
+    } else {
+      bottom = getY(MIDDLE) + INTER_CLEF;
+    }
+    return bottom;
+  }
+
+  private Context getContext(RandomAccessList<Note> notes, int midPoint) {
+    NoteType noteType;
+    int lowestMidiNote = Midi.MAX_VALUE;
+    int highestMidiNote = 0;
+    long totalDuration = 0;
+    for (Note note : notes) {
+      totalDuration += note.getDuration();
+      int midiNote = note.getMidiNote();
+      lowestMidiNote = Math.min(midiNote, lowestMidiNote);
+      highestMidiNote = Math.max(midiNote, highestMidiNote);
+    }
+    Stem stem;
+    if (lowestMidiNote < midPoint) {
+      stem = Stem.UP;
+    } else {
+      stem = Stem.DOWN;
+    }
+    long averageDuration = totalDuration / notes.size();
+    if (averageDuration < Default.TICKS_PER_BEAT / 2 + Default.GAP_BEAT_UNIT) {
+      noteType = NoteType.EIGHTH;
+    } else if (averageDuration < Default.TICKS_PER_BEAT + Default.GAP_BEAT_UNIT) {
+      noteType = NoteType.QUARTER;
+    } else if (averageDuration < Default.TICKS_PER_BEAT * 2 + Default.GAP_BEAT_UNIT) {
+      noteType = NoteType.HALF;
+    } else {
+      noteType = NoteType.WHOLE;
+    }
+    Context context = new Context(noteType, stem, lowestMidiNote, highestMidiNote);
+    return context;
+  }
+
+  private Svg getStaff(long duration) {
+    int top = getTop();
+    int bottom = getBottom();
+    int width = scale(duration);
+    Svg staff = new Svg(0, top, width, bottom);
+    for (int i = 0; i < TREBLE_MIDI_NOTES.length; i++) {
+      int y = getY(TREBLE_MIDI_NOTES[i]);
+      staff.add(new Line(0, y, width, y));
+    }
+    for (int i = 0; i < BASS_MIDI_NOTES.length; i++) {
+      int y = getY(BASS_MIDI_NOTES[i]);
+      staff.add(new Line(0, y, width, y));
+    }
+    return staff;
+  }
+
+  private int getTop() {
+    int top;
+    int highestMidiNote = song.getHighestMidiNote();
+    if (highestMidiNote < MIDDLE) {
+      top = getY(MIDDLE) + RADIUS;
+    } else {
+      top = getY(Math.max(highestMidiNote, TREBLE_MIDI_NOTES[4])) - RADIUS;
+    }
+    return top;
+  }
+
   private int getY(int midiNote) {
     int y;
     if (midiNote < MIDDLE) {
@@ -239,55 +402,6 @@ public class Notator {
       y = TOP + ((POSITION[HIGHEST] - POSITION[midiNote]) * RADIUS);
     }
     return y;
-  }
-
-  private void plotNotes(Svg svg, long firstTick, RandomAccessList<Note> notes, int midPoint) {
-    int lowestMidiNote = Midi.MAX_VALUE;
-    int highestMidiNote = 0;
-    long totalDuration = 0;
-    for (Note note : notes) {
-      int midiNote = note.getMidiNote();
-      lowestMidiNote = Math.min(midiNote, lowestMidiNote);
-      highestMidiNote = Math.max(midiNote, highestMidiNote);
-      int noteX = scale(firstTick);
-      int noteY = getY(midiNote);
-      if (LEDGER[midiNote]) {
-        svg.add(new Line(noteX - LEDGER_WIDTH, noteY, noteX + LEDGER_WIDTH, noteY));
-      }
-      boolean isSharp = isSharp(midiNote);
-      if (isSharp) {
-        int left = noteX - 4 * RADIUS;
-        int right = noteX - 2 * RADIUS;
-        int top = noteY - RADIUS / 2;
-        int bottom = noteY + RADIUS / 2;
-        svg.add(new Line(left, top, right, top));
-        svg.add(new Line(left, bottom, right, bottom));
-        svg.add(new Line(left + RADIUS / 2, top - RADIUS / 2, left + RADIUS / 2, bottom + RADIUS / 2));
-        svg.add(new Line(left + 3 * RADIUS / 2, top - RADIUS / 2, left + 3 * RADIUS / 2, bottom + RADIUS / 2));
-      }
-      long duration = note.getDuration();
-      totalDuration += duration;
-      if (duration < 1800) {
-        svg.add(new Circle(noteX, noteY, RADIUS, CLOSED));
-      } else {
-        svg.add(new Circle(noteX, noteY, RADIUS, OPEN));
-      }
-    }
-    if (notes.size() > 0) {
-      long averageDuration = totalDuration / notes.size();
-      if (averageDuration < 1900) {
-        if (lowestMidiNote < midPoint) {
-          int x = scale(firstTick) + RADIUS;
-          svg.add(new Line(x, getY(highestMidiNote) - 5 * RADIUS, x, getY(lowestMidiNote)));
-          if (averageDuration < 600) {
-            svg.add(new Line(x, getY(highestMidiNote) - 5 * RADIUS, x + RADIUS, getY(highestMidiNote) - 3 * RADIUS));
-          }
-        } else {
-          int x = scale(firstTick) - RADIUS;
-          svg.add(new Line(x, getY(highestMidiNote), x, getY(lowestMidiNote) + 5 * RADIUS));
-        }
-      }
-    }
   }
 
   private int scale(long wordTick) {
