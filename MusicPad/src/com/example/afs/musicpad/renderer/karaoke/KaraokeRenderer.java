41131f41baabf4684b7658c21fdd7c8ed106ed1c
Tony Stuart <anthonyfstuart@gmail.com>
2017-09-09 07:59:36 -0700

    Factor SongRenderer base class out of KaraokeRenderer and StaffRenderer

--- MusicPad/src/com/example/afs/musicpad/renderer/karaoke/KaraokeRenderer.java
+++ MusicPad/src/com/example/afs/musicpad/renderer/karaoke/KaraokeRenderer.java
@@ -13,20 +13,15 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NavigableMap;
-import java.util.TreeMap;
 
 import com.example.afs.musicpad.html.Option;
 import com.example.afs.musicpad.html.Template;
 import com.example.afs.musicpad.message.Message;
 import com.example.afs.musicpad.message.OnChannelDetails;
-import com.example.afs.musicpad.message.OnChannelUpdate;
 import com.example.afs.musicpad.message.OnChannels;
-import com.example.afs.musicpad.message.OnCommand;
-import com.example.afs.musicpad.message.OnDeviceDetached;
 import com.example.afs.musicpad.message.OnKaraokePrompter;
 import com.example.afs.musicpad.message.OnMidiFiles;
 import com.example.afs.musicpad.message.OnPickChannel;
-import com.example.afs.musicpad.message.OnRenderSong;
 import com.example.afs.musicpad.message.OnSampleChannel;
 import com.example.afs.musicpad.message.OnSampleSong;
 import com.example.afs.musicpad.message.OnSongDetails;
@@ -35,28 +30,19 @@ import com.example.afs.musicpad.message.OnTemplates;
 import com.example.afs.musicpad.midi.Instruments;
 import com.example.afs.musicpad.midi.Midi;
 import com.example.afs.musicpad.playable.Playable;
-import com.example.afs.musicpad.playable.PlayableMap;
+import com.example.afs.musicpad.renderer.SongRenderer;
 import com.example.afs.musicpad.song.Song;
-import com.example.afs.musicpad.task.BrokerTask;
 import com.example.afs.musicpad.util.Broker;
 import com.example.afs.musicpad.util.RandomAccessList;
 
-public class KaraokeRenderer extends BrokerTask<Message> {
-
-  private Song song;
-  private NavigableMap<Integer, Integer> deviceChannelAssignments = new TreeMap<>();
-  private NavigableMap<Integer, RandomAccessList<Playable>> devicePlayables = new TreeMap<>();
+public class KaraokeRenderer extends SongRenderer {
 
   public KaraokeRenderer(Broker<Message> broker) {
     super(broker);
-    subscribe(OnCommand.class, message -> doCommand(message));
     subscribe(OnMidiFiles.class, message -> doMidiFiles(message));
-    subscribe(OnRenderSong.class, message -> doRenderSong(message));
     subscribe(OnSampleSong.class, message -> doSampleSong(message));
     subscribe(OnPickChannel.class, message -> doPickChannel(message));
     subscribe(OnSampleChannel.class, message -> doSampleChannel(message));
-    subscribe(OnChannelUpdate.class, message -> doChannelUpdate(message));
-    subscribe(OnDeviceDetached.class, message -> doDeviceDetached(message));
   }
 
   @Override
@@ -65,45 +51,11 @@ public class KaraokeRenderer extends BrokerTask<Message> {
     publishTemplates();
   }
 
-  private boolean allDevicePlayablesAvailable() {
-    if (deviceChannelAssignments.size() == 0) {
-      return false;
-    }
-    for (Integer deviceIndex : deviceChannelAssignments.keySet()) {
-      if (!devicePlayables.containsKey(deviceIndex)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  private void doChannelUpdate(OnChannelUpdate message) {
-    int deviceIndex = message.getDeviceIndex();
-    PlayableMap playableMap = message.getPlayableMap();
-    RandomAccessList<Playable> playables = playableMap.getPlayables();
-    devicePlayables.put(deviceIndex, playables);
-    renderWhenReady();
-  }
-
-  private void doCommand(OnCommand message) {
-    switch (message.getCommand()) {
-    case SELECT_SONG:
-      doSelectSong(message.getParameter());
-      break;
-    default:
-      break;
-    }
-  }
-
-  private void doDeviceDetached(OnDeviceDetached message) {
-    if (devicePlayables.size() == 1) {
-      System.err.println("KaraokeRenderer: suppressing removal of last playable for testing purposes");
-      return;
-    }
-    int deviceIndex = message.getDeviceIndex();
-    devicePlayables.remove(deviceIndex);
-    deviceChannelAssignments.remove(deviceIndex);
-    renderWhenReady();
+  @Override
+  protected void render(Song song, NavigableMap<Integer, RandomAccessList<Playable>> devicePlayables, NavigableMap<Integer, Integer> deviceChannelAssignments) {
+    KarokeNotator karokeNotator = new KarokeNotator(song, devicePlayables);
+    String html = karokeNotator.render();
+    publish(new OnKaraokePrompter(html));
   }
 
   private void doMidiFiles(OnMidiFiles message) {
@@ -117,13 +69,6 @@ public class KaraokeRenderer extends BrokerTask<Message> {
     ChannelSelector channelSelector = new ChannelSelector(message.getSong(), message.getDeviceIndex(), message.getDeviceChannelAssignments());
     String html = channelSelector.render();
     publish(new OnChannels(html));
-  }
-
-  private void doRenderSong(OnRenderSong message) {
-    song = message.getSong();
-    deviceChannelAssignments.clear();
-    deviceChannelAssignments.putAll(message.getDeviceChannelAssignments());
-    renderWhenReady();
   }
 
   private void doSampleChannel(OnSampleChannel message) {
@@ -141,10 +86,6 @@ public class KaraokeRenderer extends BrokerTask<Message> {
     publish(new OnSongDetails(html));
   }
 
-  private void doSelectSong(int songIndex) {
-    deviceChannelAssignments.clear();
-  }
-
   private String getProgramOptions() {
     Template template = new Template("#program-options");
     for (int i = 0; i < Midi.PROGRAMS; i++) {
@@ -159,14 +100,6 @@ public class KaraokeRenderer extends BrokerTask<Message> {
     List<String> templates = new LinkedList<>();
     templates.add(getProgramOptions());
     getBroker().publish(new OnTemplates(templates));
-  }
-
-  private void renderWhenReady() {
-    if (song != null && allDevicePlayablesAvailable()) {
-      KarokeNotator karokeNotator = new KarokeNotator(song, devicePlayables);
-      String html = karokeNotator.render();
-      publish(new OnKaraokePrompter(html));
-    }
   }
 
 }
