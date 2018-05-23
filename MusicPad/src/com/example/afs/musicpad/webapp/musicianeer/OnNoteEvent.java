1a972a85035465faef5c57021ce43ab4ccf94476
Tony Stuart <anthonyfstuart@gmail.com>
2018-05-23 06:08:50 -0700

    Switch from input/review queues to single note event array

--- MusicPad/src/com/example/afs/musicpad/webapp/musicianeer/OnNoteEvent.java
+++ MusicPad/src/com/example/afs/musicpad/webapp/musicianeer/OnNoteEvent.java
@@ -1,4 +1,4 @@
-// Copyright 2017 Anthony F. Stuart - All rights reserved.
+// Copyright 2018 Anthony F. Stuart - All rights reserved.
 //
 // This program and the accompanying materials are made available
 // under the terms of the GNU General Public License. For other license
@@ -9,111 +9,24 @@
 
 package com.example.afs.musicpad.webapp.musicianeer;
 
-import com.example.afs.musicpad.song.Note;
 import com.example.afs.musicpad.task.Message;
+import com.example.afs.musicpad.transport.NoteEvent;
 
-public class OnNoteEvent implements Message, Comparable<OnNoteEvent> {
+public class OnNoteEvent implements Message {
 
-  public enum Type { // Ordinal is used by compareTo
-    TICK, //
-    CUE_NOTE_ON, //
-    NOTE_OFF, //
-    NOTE_ON, // 
+  private NoteEvent noteEvent;
+
+  public OnNoteEvent(NoteEvent noteEvent) {
+    this.noteEvent = noteEvent;
   }
 
-  private Type type;
-  private long tick;
-  private int beatsPerMinute;
-  private Note note;
-
-  public OnNoteEvent(Type type, long tick, int beatsPerMinute) {
-    this.type = type;
-    this.tick = tick;
-    this.beatsPerMinute = beatsPerMinute;
-  }
-
-  public OnNoteEvent(Type type, long tick, Note note) {
-    this.type = type;
-    this.tick = tick;
-    this.note = note;
-    this.beatsPerMinute = note.getBeatsPerMinute();
-  }
-
-  @Override
-  public int compareTo(OnNoteEvent that) {
-    long deltaTick = this.tick - that.tick;
-    if (deltaTick != 0) {
-      return deltaTick < 0 ? -1 : +1;
-    }
-    int deltaType = this.type.ordinal() - that.type.ordinal();
-    if (deltaType != 0) {
-      return deltaType;
-    }
-    if (this.note != null && that.note != null) {
-      int deltaNote = this.note.compareTo(that.note);
-      if (deltaNote != 0) {
-        return deltaNote;
-      }
-    }
-    return 0;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    OnNoteEvent other = (OnNoteEvent) obj;
-    if (note == null) {
-      if (other.note != null) {
-        return false;
-      }
-    } else if (!note.equals(other.note)) {
-      return false;
-    }
-    if (tick != other.tick) {
-      return false;
-    }
-    if (type != other.type) {
-      return false;
-    }
-    return true;
-  }
-
-  public int getBeatsPerMinute() {
-    return beatsPerMinute;
-  }
-
-  public Note getNote() {
-    return note;
-  }
-
-  public long getTick() {
-    return tick;
-  }
-
-  public Type getType() {
-    return type;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((note == null) ? 0 : note.hashCode());
-    result = prime * result + (int) (tick ^ (tick >>> 32));
-    result = prime * result + ((type == null) ? 0 : type.hashCode());
-    return result;
+  public NoteEvent getNoteEvent() {
+    return noteEvent;
   }
 
   @Override
   public String toString() {
-    return "NoteEvent [note=" + note + ", tick=" + tick + "]";
+    return "OnNoteEvent [noteEvent=" + noteEvent + "]";
   }
+
 }
--- MusicPad/src/com/example/afs/musicpad/webapp/musicianeer/Transport.java
+++ MusicPad/src/com/example/afs/musicpad/webapp/musicianeer/Transport.java
@@ -9,29 +9,23 @@
 
 package com.example.afs.musicpad.webapp.musicianeer;
 
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.util.Collections;
 
 import com.example.afs.fluidsynth.Synthesizer;
 import com.example.afs.musicpad.midi.Midi;
 import com.example.afs.musicpad.song.Default;
 import com.example.afs.musicpad.song.Note;
-import com.example.afs.musicpad.task.Message;
 import com.example.afs.musicpad.task.MessageBroker;
 import com.example.afs.musicpad.task.MessageTask;
+import com.example.afs.musicpad.transport.NoteEvent;
+import com.example.afs.musicpad.transport.NoteEvent.Type;
+import com.example.afs.musicpad.util.DirectList;
+import com.example.afs.musicpad.util.RandomAccessList;
 import com.example.afs.musicpad.util.Range;
 import com.example.afs.musicpad.util.Tick;
 import com.example.afs.musicpad.util.Velocity;
-import com.example.afs.musicpad.webapp.musicianeer.OnNoteEvent.Type;
 
 public class Transport extends MessageTask {
-
-  public interface NoteEventProcessor {
-    void processNoteEvent(OnNoteEvent noteEvent);
-  }
 
   public enum Whence {
     RELATIVE, ABSOLUTE
@@ -44,13 +38,14 @@ public class Transport extends MessageTask {
   public static final int DEFAULT_MASTER_PROGRAM_OFF = Midi.MAX_VALUE;
   public static final long INITIALIZE_ON_NEXT_EVENT = -1;
 
+  private int index;
   private int currentTransposition;
   private int masterProgram = DEFAULT_MASTER_PROGRAM_OFF;
   private int percentVelocity = DEFAULT_PERCENT_VELOCITY;
   private int[] currentPrograms = new int[Midi.CHANNELS];
 
   private Synthesizer synthesizer;
-  private Deque<OnNoteEvent> reviewQueue = new LinkedList<>();
+  private RandomAccessList<NoteEvent> noteEvents = new DirectList<>();
   private OnSetAccompanimentType.AccompanimentType accompanimentType = OnSetAccompanimentType.AccompanimentType.FULL;
 
   private long baseTick;
@@ -58,43 +53,12 @@ public class Transport extends MessageTask {
   private int percentTempo = 100;
   private int appliedPercentTempo = percentTempo;
   private boolean isPaused;
-  private PriorityBlockingQueue<Message> inputQueue;
 
   public Transport(MessageBroker messageBroker, Synthesizer synthesizer) {
     super(messageBroker);
     this.synthesizer = synthesizer;
     setPercentGain(DEFAULT_PERCENT_GAIN);
-    subscribe(OnNoteEvent.class, noteEvent -> processNoteEvent(noteEvent));
-  }
-
-  public void allNotesOff() {
-    synthesizer.allNotesOff();
-  }
-
-  public void clear() {
-    tsGetInputQueue().clear();
-    setPaused(false);
-    resetAll();
-    synthesizer.allNotesOff();
-    reviewQueue.clear();
-  }
-
-  public long getEventTimeMillis(long noteTick, int beatsPerMinute) {
-    if (baseTimeMillis == INITIALIZE_ON_NEXT_EVENT || noteTick < baseTick) {
-      baseTimeMillis = System.currentTimeMillis();
-    }
-    long deltaTick = noteTick - baseTick;
-    deltaTick = (deltaTick * 100) / appliedPercentTempo;
-    long deltaMillis = Tick.convertTickToMillis(beatsPerMinute, deltaTick);
-    long eventTimeMillis = baseTimeMillis + deltaMillis;
-    // Update base values to handle changes in beats per minute
-    baseTick = noteTick;
-    baseTimeMillis = eventTimeMillis;
-    return eventTimeMillis;
-  }
-
-  public long getEventTimeMillis(OnNoteEvent noteEvent) {
-    return getEventTimeMillis(noteEvent.getTick(), noteEvent.getBeatsPerMinute());
+    subscribe(OnNoteEvent.class, message -> doNoteEvent(message));
   }
 
   public int getPercentGain() {
@@ -109,12 +73,9 @@ public class Transport extends MessageTask {
     return isPaused;
   }
 
-  public boolean isPlaying() {
-    return !isPaused && tsGetInputQueue().size() > 0;
-  }
-
   public void pause() {
-    setPaused(true);
+    isPaused = true;
+    baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
     synthesizer.allNotesOff();
   }
 
@@ -129,38 +90,34 @@ public class Transport extends MessageTask {
       long duration = note.getDuration();
       if (firstTick == -1) {
         firstTick = beginTick;
-        setBaseTick(firstTick);
+        baseTick = firstTick;
+        baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
       }
       long beginTickRoundedUp = ((beginTick + 1) / Default.RESOLUTION) * Default.RESOLUTION;
       while (metronomeTick < beginTickRoundedUp) {
         metronomeTick = ((metronomeTick + Default.RESOLUTION + 1) / Default.RESOLUTION) * Default.RESOLUTION;
-        inputQueue.add(new OnNoteEvent(Type.TICK, metronomeTick, beatsPerMinute));
+        noteEvents.add(new NoteEvent(Type.TICK, metronomeTick, beatsPerMinute));
       }
       long endTick = beginTick + duration;
       lastTick = Math.max(lastTick, endTick);
-      inputQueue.add(new OnNoteEvent(Type.NOTE_ON, beginTick, note));
-      inputQueue.add(new OnNoteEvent(Type.CUE_NOTE_ON, beginTick - 1024, note));
-      inputQueue.add(new OnNoteEvent(Type.NOTE_OFF, endTick, note));
+      noteEvents.add(new NoteEvent(Type.NOTE_ON, beginTick, note));
+      noteEvents.add(new NoteEvent(Type.CUE_NOTE_ON, beginTick - 1024, note));
+      noteEvents.add(new NoteEvent(Type.NOTE_OFF, endTick, note));
       beatsPerMinute = note.getBeatsPerMinute();
     }
     long lastTickRoundedUp = ((lastTick + 1) / Default.RESOLUTION) * Default.RESOLUTION;
     while (metronomeTick < lastTickRoundedUp) {
       metronomeTick = ((metronomeTick + Default.RESOLUTION + 1) / Default.RESOLUTION) * Default.RESOLUTION;
-      inputQueue.add(new OnNoteEvent(Type.TICK, metronomeTick, beatsPerMinute));
+      noteEvents.add(new NoteEvent(Type.TICK, metronomeTick, beatsPerMinute));
     }
-  }
-
-  public void resetAll() {
-    this.baseTick = 0;
-    this.baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
-  }
-
-  public void resetBaseTime() {
-    this.baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
+    Collections.sort(noteEvents);
+    schedule();
   }
 
   public void resume() {
-    setPaused(false);
+    isPaused = false;
+    baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
+    schedule();
   }
 
   public void seek(long tick, Whence whence) {
@@ -179,38 +136,16 @@ public class Transport extends MessageTask {
       throw new UnsupportedOperationException();
     }
     if (newTick > currentTick) {
-      boolean done = false;
-      do {
-        Message message = inputQueue.poll();
-        if (message == null) {
-          done = true;
-        }
-        if (message instanceof OnNoteEvent) {
-          OnNoteEvent onNoteEvent = (OnNoteEvent) message;
-          if (onNoteEvent.getTick() < newTick) {
-            reviewQueue.add(onNoteEvent);
-          } else {
-            inputQueue.add(onNoteEvent);
-            done = true;
-          }
-        } else {
-          // TODO: Dispatch the message?
-        }
-      } while (!done);
+      while (noteEvents.get(index).getTick() < newTick && index < noteEvents.size()) {
+        index++;
+      }
     } else {
-      boolean isInRange = true;
-      Iterator<OnNoteEvent> iterator = reviewQueue.descendingIterator();
-      while (iterator.hasNext() && isInRange) {
-        OnNoteEvent noteEvent = iterator.next();
-        long noteEventTick = noteEvent.getTick();
-        isInRange = noteEventTick > newTick;
-        if (isInRange) {
-          inputQueue.add(noteEvent);
-          iterator.remove();
-        }
+      while (noteEvents.get(index).getTick() > newTick && index > 0) {
+        index--;
       }
     }
-    setBaseTick(newTick);
+    baseTick = newTick;
+    baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
     if (wasPlaying) {
       resume();
     } else {
@@ -228,23 +163,8 @@ public class Transport extends MessageTask {
     }
   }
 
-  public void setBaseTimeMillis(long baseTimeMillis) {
-    this.baseTimeMillis = baseTimeMillis;
-  }
-
   public void setCurrentTransposition(int currentTransposition) {
     this.currentTransposition = currentTransposition;
-  }
-
-  public void setMasterProgram(int masterProgram) {
-    this.masterProgram = masterProgram;
-  }
-
-  public void setPaused(boolean isPaused) {
-    this.isPaused = isPaused;
-    if (isPaused) {
-      resetBaseTime();
-    }
   }
 
   public void setPercentGain(int gain) {
@@ -268,28 +188,49 @@ public class Transport extends MessageTask {
     clear();
   }
 
-  @Override
-  protected BlockingQueue<Message> createInputQueue() {
-    inputQueue = new PriorityBlockingQueue<>();
-    return inputQueue;
+  private void clear() {
+    noteEvents.clear();
+    tsGetInputQueue().clear();
+    synthesizer.allNotesOff();
+    isPaused = false;
+    baseTick = 0;
+    baseTimeMillis = INITIALIZE_ON_NEXT_EVENT;
   }
 
-  @Override
-  protected void processMessage(Message message) throws InterruptedException {
-    if (message instanceof OnNoteEvent) {
-      long currentTimestamp = System.currentTimeMillis();
-      long eventTimestamp = getEventTimeMillis((OnNoteEvent) message);
-      if (eventTimestamp > currentTimestamp) {
-        long sleepInterval = eventTimestamp - currentTimestamp;
+  private void doNoteEvent(OnNoteEvent message) {
+    long currentTimestamp = System.currentTimeMillis();
+    NoteEvent noteEvent = message.getNoteEvent();
+    long eventTimestamp = getEventTimeMillis(noteEvent);
+    if (eventTimestamp > currentTimestamp) {
+      long sleepInterval = eventTimestamp - currentTimestamp;
+      try {
         Thread.sleep(sleepInterval);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
       }
-      super.processMessage(message);
-      while (isPaused && !isTerminated()) {
-        Thread.sleep(250);
-      }
-    } else {
-      super.processMessage(message);
     }
+    processNoteEvent(noteEvent);
+    if (!isPaused) {
+      schedule();
+    }
+  }
+
+  private long getEventTimeMillis(long noteTick, int beatsPerMinute) {
+    if (baseTimeMillis == INITIALIZE_ON_NEXT_EVENT) {
+      baseTimeMillis = System.currentTimeMillis();
+    }
+    long deltaTick = noteTick - baseTick;
+    deltaTick = (deltaTick * 100) / appliedPercentTempo;
+    long deltaMillis = Tick.convertTickToMillis(beatsPerMinute, deltaTick);
+    long eventTimeMillis = baseTimeMillis + deltaMillis;
+    // Update base values to handle changes in beats per minute
+    baseTick = noteTick;
+    baseTimeMillis = eventTimeMillis;
+    return eventTimeMillis;
+  }
+
+  private long getEventTimeMillis(NoteEvent noteEvent) {
+    return getEventTimeMillis(noteEvent.getTick(), noteEvent.getBeatsPerMinute());
   }
 
   private int getTransposedMidiNote(Note note, int channel) {
@@ -300,7 +241,11 @@ public class Transport extends MessageTask {
     return midiNote;
   }
 
-  private void processNoteEvent(OnNoteEvent noteEvent) {
+  private boolean isPlaying() {
+    return !isPaused && index < noteEvents.size();
+  }
+
+  private void processNoteEvent(NoteEvent noteEvent) {
     Note note = noteEvent.getNote();
     switch (noteEvent.getType()) {
 
@@ -360,12 +305,13 @@ public class Transport extends MessageTask {
     default:
       throw new UnsupportedOperationException();
     }
-    reviewQueue.add(noteEvent);
   }
 
-  private void setBaseTick(long baseTick) {
-    this.baseTick = baseTick;
-    resetBaseTime();
+  private void schedule() {
+    if (index < noteEvents.size()) {
+      NoteEvent noteEvent = noteEvents.get(index++);
+      tsGetInputQueue().add(new OnNoteEvent(noteEvent));
+    }
   }
 
 }
