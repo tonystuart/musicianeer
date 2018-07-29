542a6d94c00e53370bfee7ba77227977e760ba5f
Tony Stuart <anthonyfstuart@gmail.com>
2018-07-29 15:24:52 -0700

    Use note-to-LED map instead of algorithm

--- Musicianeer/arduino/PianoBar-SK9822/PianoBar-SK9822.ino
+++ Musicianeer/arduino/PianoBar-SK9822/PianoBar-SK9822.ino
@@ -5,8 +5,10 @@
 #define DATA_PIN 16
 #define CLOCK_PIN 15
 
-#define LOWEST_NOTE 35 // one less than Musicianeer
+#define LOWEST_NOTE 36
 #define HIGHEST_NOTE 84
+
+#define NOTE_COUNT ((HIGHEST_NOTE - LOWEST_NOTE) + 1) // +1 to go from index to count
 
 #define INACTIVITY_THRESHOLD (1000L * 60 * 2)
 //#define INACTIVITY_THRESHOLD (1000 * 10)
@@ -18,9 +20,11 @@ uint8_t hue = 0;
 uint32_t inactivity_timer = 0;
 bool is_display_spectrum = true;
 
+uint8_t note_to_led_map[NOTE_COUNT] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96 };
+
 void setup() { 
 		Serial.begin(57600);
-		FastLED.addLeds<SK9822,DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
+		FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
 		LEDS.setBrightness(84);
 		Serial.print("Compiled at ");
 		Serial.print(__TIME__);
@@ -77,16 +81,9 @@ void display_spectrum() {
 void display_note_off(midiEventPacket_t message) {
 		uint8_t channel = message.byte1 & 0xf;
 		uint8_t midi_note = message.byte2;
-		//Serial.print("NOTE_OFF: channel=");
-		//Serial.print(channel);
-		//Serial.print(", midi_note=");
-		//Serial.println(midi_note);
 		if (midi_note >= LOWEST_NOTE && midi_note <= HIGHEST_NOTE) {
-				uint8_t led_index = message.byte2 - LOWEST_NOTE;
-				led_index *= 2; // for SK9822
-				led_index -= (led_index / 24);
-				//leds[led_index] = 0;
-				leds[led_index + 1] = 0;
+				uint8_t led_index = note_to_led_map[message.byte2 - LOWEST_NOTE];
+				leds[led_index] = 0;
 				FastLED.show(); 
 		}
 }
@@ -95,41 +92,19 @@ void display_note_on(midiEventPacket_t message) {
 		uint8_t channel = message.byte1 & 0xf;
 		uint8_t midi_note = message.byte2;
 		uint8_t velocity = message.byte3;
-		//Serial.print("NOTE_ON: channel=");
-		//Serial.print(channel);
-		//Serial.print(", midi_note=");
-		//Serial.print(midi_note);
-		//Serial.print(", velocity=");
-		//Serial.println(velocity);
 		if (midi_note >= LOWEST_NOTE && midi_note <= HIGHEST_NOTE) {
-				uint8_t led_index = message.byte2 - LOWEST_NOTE;
+				uint8_t led_index = note_to_led_map[message.byte2 - LOWEST_NOTE];
 				uint8_t i = channel / 4;
 				uint8_t j = channel % 4;
 				uint32_t degrees = (j * 90) + ((i * 90) / 4);
 				uint8_t hue = (degrees * 255) / 360;
 				uint8_t brightness = ((velocity * 255) / 127);
-				//Serial.print("channel="+channel);
-				//Serial.print(channel);
-				//Serial.print(", i=");
-				//Serial.print(i);
-				//Serial.print(", j=");
-				//Serial.print(j);
-				//Serial.print(", degrees=");
-				//Serial.print(degrees);
-				//Serial.print(", hue=");
-				//Serial.print(hue);
-				//Serial.print(", brightness=");
-				//Serial.println(brightness);
-				led_index *= 2; // for SK9822
-				led_index -= (led_index / 24);
-				//hsv2rgb_spectrum(CHSV(hue, 255, brightness), leds[led_index]);
-				hsv2rgb_spectrum(CHSV(hue, 255, brightness), leds[led_index+1]);
+				hsv2rgb_spectrum(CHSV(hue, 255, brightness), leds[led_index]);
 				FastLED.show(); 
 		}
 }
 
 void print_unsupported_event(midiEventPacket_t message) {
-		Serial.print("Received: ");
 		Serial.print(message.header, HEX);
 		Serial.print("-");
 		Serial.print(message.byte1, HEX);
@@ -138,3 +113,11 @@ void print_unsupported_event(midiEventPacket_t message) {
 		Serial.print("-");
 		Serial.println(message.byte3, HEX);
 }
+
+void show_note_to_led_map() {
+		for (int i = 0; i < NOTE_COUNT; i++) {
+			leds[note_to_led_map[i]] = CRGB(64,0,0);
+			FastLED.show();
+		}
+}
+
