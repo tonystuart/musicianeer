6bd9409fbdb2fcb4d975f9d3f3a16dd4526f7ace
Tony Stuart <anthonyfstuart@gmail.com>
2018-08-15 21:28:39 -0700

    Increase row height, reduce font size, emphasize current measure

--- Musicianeer/src/com/example/afs/musicianeer/main/MusicianeerView.java
+++ Musicianeer/src/com/example/afs/musicianeer/main/MusicianeerView.java
@@ -481,7 +481,8 @@ public class MusicianeerView extends ShadowDomBuilder {
     int measureCount = noteCountsByMeasure.length;
     Svg svg = new Svg(Svg.Type.SCALE_TO_FIT, 0, 0, measureCount, PROFILE_HEIGHT, ".profile", ".channel-" + channel);
     for (int i = 0; i < measureCount; i++) {
-      int height = noteCountsByMeasure[i] / 2;
+      int clippedNoteCountForMeasure = Math.min(PROFILE_HEIGHT - 4, noteCountsByMeasure[i]);
+      int height = clippedNoteCountForMeasure / 2;
       int y1 = (PROFILE_HEIGHT / 2) - height;
       int y2 = (PROFILE_HEIGHT / 2) + height;
       String id = "#profile-" + channel + "-" + i;
