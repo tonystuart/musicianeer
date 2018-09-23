7196ccdc882cff083a8ee8f6b5dcd07f608f4e6d
Tony Stuart <anthonyfstuart@gmail.com>
2018-09-23 09:15:28 -0700

    Add copyright message

--- Musicianeer/jni/com_example_afs_jni_FluidSynth.c
+++ Musicianeer/jni/com_example_afs_jni_FluidSynth.c
@@ -1,3 +1,13 @@
+/* Copyright 2018 Anthony F. Stuart - All rights reserved.
+//
+// This program and the accompanying materials are made available
+// under the terms of the GNU General Public License. For other license
+// options please contact the copyright owner.
+//
+// This program is made available on an "as is" basis, without
+// warranties or conditions of any kind, either express or implied.
+*/
+
 #include <jni.h>
 #include <fluidsynth.h>
 #include <linux/input.h>
